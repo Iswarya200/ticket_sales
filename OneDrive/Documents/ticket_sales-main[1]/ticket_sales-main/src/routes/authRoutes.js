04db3dd1e999ae27c2892aad081ca4d0390e84fa
ChaturyaMullapudi <22je0595@iitism.ac.in>
2025-04-06 01:41:55 +0530

    12 commit

--- OneDrive/Documents/ticket_sales-main[1]/ticket_sales-main/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { register, login, getProfile } = require('../controllers/authController');
-
-router.post('/register', register);
-router.post('/login', login);
-router.get('/profile', getProfile);
-
-module.exports = router;
